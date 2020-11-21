36a661998a39f21da1397c164da7cd96da2d5ce2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-21 16:42:21 +0900

    feat(file-dialog.ts): split functions

--- src/file-dialog.ts
+++ src/file-dialog.ts
@@ -21,37 +21,54 @@ export const fileDialog = <T extends boolean = false>(
   parameters?: Partial<Parameters<T>>
 ): Promise<PickFile<T>> => {
   const { accept, multiple } = { ...DEFAULT_PARAMETERS, ...parameters }
+  const input = createFileInputElement({
+    multiple,
+    accept: Array.isArray(accept) ? joinArrayString(accept) : accept
+  })
+  return new Promise<PickFile<T>>((resolve) => {
+    input.onchange = () => {
+      resolve(resolver(input.files, multiple as T))
+      input.remove()
+    }
+    input.click()
+  })
+}
+
+export const resolver = <T extends boolean>(
+  fileList: HTMLInputElement['files'],
+  multiple: T
+) => {
+  return new Promise<PickFile<T>>((resolve, reject) => {
+    if (!fileList) return reject(new NotReceiveFileList())
+    const file = getFileListObj(fileList, multiple)
+    if (!file) return reject(new UnknownError())
+
+    resolve(file)
+  })
+}
+
+export const getFileListObj = <T extends boolean>(
+  files: NonNullable<HTMLInputElement['files']>,
+  multiple: T
+): PickFile<T> | null => {
+  if (multiple) {
+    return <PickFile<T>>files
+  } else if (files.length === 1) {
+    // It is not assume files length is 1.
+
+    return <PickFile<T>>files[0]
+  } else {
+    return null
+  }
+}
+
+export const createFileInputElement = ({
+  accept,
+  multiple
+}: Pick<HTMLInputElement, 'accept' | 'multiple'>): HTMLInputElement => {
   const input = document.createElement('input')
   input.type = 'file'
   input.multiple = multiple
-  input.accept = Array.isArray(accept) ? joinArrayString(accept) : accept
-
-  return new Promise<PickFile<T>>((resolve, reject) => {
-    const onChange = (): void => {
-      resolver()
-      input.remove()
-    }
-
-    input.onchange = onChange
-
-    const resolver = (): void => {
-      const { files } = input
-      if (!files) {
-        reject(new NotReceiveFileList())
-        return
-      }
-
-      if (multiple) {
-        resolve(<PickFile<T>>files)
-      } else if (files.length === 1) {
-        // It is not assume files length is 1.
-
-        resolve(<PickFile<T>>files[0])
-      } else {
-        reject(new UnknownError())
-      }
-    }
-
-    input.click()
-  })
+  input.accept = accept
+  return input
 }
