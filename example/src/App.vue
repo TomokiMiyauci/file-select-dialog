55d97523db2f4b20e2b822c384b408fb50e20405
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-21 23:17:59 +0900

    chore: initial example

---
+++ example/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    id="button"
+    ref="button"
+    type="file"
+    style="position: fixed; top: 0; left: 0"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { fileDialog } from 'file-select-dialog'
+
+  export default defineComponent({
+    name: 'App',
+
+    setup() {
+      const onClick = async () => {
+        const file = await fileDialog({ multiple: false, accept: '.png,.pdf' })
+        console.log(file)
+      }
+
+      return { onClick }
+    },
+  })
+</script>
