de7de62b4ae4664ef547cbd0e87a1429c24a6f84
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-29 03:30:41 +0900

    perf(rollup.config.ts): change output format

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,28 @@
-import pkg from './package.json'
+import { main, unpkg, name, module } from './package.json'
 import typescript from 'rollup-plugin-typescript2'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import { terser } from 'rollup-plugin-terser'
+import camelCase from 'lodash.camelcase'
+
+const moduleName = camelCase(name)
 
 const config = {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
-      name: pkg.name,
-      extend: true,
+      file: main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: module,
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: unpkg,
+      name: moduleName,
+      format: 'iife',
       sourcemap: true
     }
   ],
