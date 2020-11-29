b9754e28ec14686a223856cb04500a2bc8aa651e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-29 15:43:15 +0900

    feat(file-dialog): add strict parameter and change default return types

--- src/file-dialog.ts
+++ src/file-dialog.ts
@@ -2,61 +2,76 @@ import { UnknownError, NotReceiveFileList } from './error'
 import { joinArrayString } from './util'
 const DEFAULT_PARAMETERS: Parameters = {
   accept: '*',
-  multiple: false
+  multiple: false,
+  strict: false
 }
 
-export interface Parameters<T extends boolean = false> {
+export interface Parameters<
+  T extends boolean = false,
+  U extends boolean = false
+> {
   accept: string | string[]
   multiple: T
+  strict: U
 }
 
-type PickFile<T extends boolean = false> = T extends true ? FileList : File
+type PickFile<
+  T extends boolean = false,
+  U extends boolean = false
+> = T extends false ? (U extends true ? File : FileList) : FileList
 
 /**
  * Call file select dialog programmatically.
  * @param {object} [parameters] optional
- * @returns {Promise} Promise <File | FileList> if multiple is true, return FileList object, if false return File object
+ * @returns {Promise} Promise <File | FileList> if strict is false, return FileList object, if multiple is false and strict is true return File object
  */
-export const fileDialog = <T extends boolean = false>(
-  parameters?: Partial<Parameters<T>>
-): Promise<PickFile<T>> => {
-  const { accept, multiple } = { ...DEFAULT_PARAMETERS, ...parameters }
+export const fileDialog = <
+  T extends boolean = false,
+  U extends boolean = false
+>(
+  parameters?: Partial<Parameters<T, U>>
+): Promise<PickFile<T, U>> => {
+  const { accept, multiple, strict } = { ...DEFAULT_PARAMETERS, ...parameters }
   const input = createFileInputElement({
     multiple,
     accept: Array.isArray(accept) ? joinArrayString(accept) : accept
   })
-  return new Promise<PickFile<T>>((resolve) => {
+  return new Promise<PickFile<T, U>>((resolve) => {
     input.onchange = () => {
-      resolve(resolver(input.files, multiple as T))
+      resolve(resolver(input.files, multiple as T, strict as U))
       input.remove()
     }
     input.click()
   })
 }
 
-export const resolver = <T extends boolean>(
+export const resolver = <T extends boolean, U extends boolean>(
   fileList: HTMLInputElement['files'],
-  multiple: T
+  multiple: T,
+  strict: U
 ) => {
-  return new Promise<PickFile<T>>((resolve, reject) => {
+  return new Promise<PickFile<T, U>>((resolve, reject) => {
     if (!fileList) return reject(new NotReceiveFileList())
-    const file = getFileListObj(fileList, multiple)
+    const file = getFileListObj(fileList, multiple, strict)
     if (!file) return reject(new UnknownError())
 
     resolve(file)
   })
 }
 
-export const getFileListObj = <T extends boolean>(
+export const getFileListObj = <T extends boolean, U extends boolean>(
   files: NonNullable<HTMLInputElement['files']>,
-  multiple: T
-): PickFile<T> | null => {
-  if (multiple) {
-    return <PickFile<T>>files
-  } else if (files.length === 1) {
-    // It is not assume files length is 1.
-
-    return <PickFile<T>>files[0]
+  multiple: T,
+  strict: U
+): PickFile<T, U> | null => {
+  if (!multiple && strict) {
+    if (files.length === 1) {
+      return <PickFile<T, U>>files[0]
+    } else {
+      return null
+    }
+  } else if (files.length) {
+    return <PickFile<T, U>>files
   } else {
     return null
   }
--- tests/unit/file-dialog.spec.ts
+++ tests/unit/file-dialog.spec.ts
@@ -29,43 +29,70 @@ describe('createFileInputElement', () => {
 
 describe('resolver', () => {
   it('should throw NotReceiveFileList when it gives files of null', async () => {
-    await expect(resolver(null, false)).rejects.toThrowError(NotReceiveFileList)
+    await expect(resolver(null, false, false)).rejects.toThrowError(
+      NotReceiveFileList
+    )
   })
 
-  it('should return FileList when it gives FileList what length is 1 and multiple is false', async () => {
+  it('should return File when it gives FileList what length is 1 and multiple is false and strict is true', async () => {
     await expect(
-      resolver(([{}] as unknown) as FileList, false)
+      resolver(([{}] as unknown) as FileList, false, true)
     ).resolves.toEqual({})
   })
 
-  it('should throw UnknownError when it gives files length is not equal 1 and multiple is false', async () => {
+  it('should return FileList when it gives FileList what length is 1 and multiple is false and strict is false', async () => {
     await expect(
-      resolver(([{}, {}] as unknown) as FileList, false)
+      resolver(([{}] as unknown) as FileList, false, false)
+    ).resolves.toEqual([{}])
+  })
+
+  it('should throw UnknownError when it gives files length is not equal 1 and multiple is false and strict is true', async () => {
+    await expect(
+      resolver(([{}, {}] as unknown) as FileList, false, true)
     ).rejects.toThrowError(UnknownError)
   })
 
   it('should return FileList when it gives FileList and multiple is true', async () => {
+    expect.assertions(2)
     await expect(
-      resolver(([{}, {}] as unknown) as FileList, true)
+      resolver(([{}, {}] as unknown) as FileList, true, false)
+    ).resolves.toEqual([{}, {}])
+
+    await expect(
+      resolver(([{}, {}] as unknown) as FileList, true, true)
     ).resolves.toEqual([{}, {}])
   })
 })
 
 describe('getFileListObj', () => {
   it('return File object when files', () => {
-    const expected = getFileListObj(([{}] as never) as FileList, false)
+    const expected = getFileListObj(([{}] as never) as FileList, false, true)
 
     expect(expected).toEqual({})
   })
 
   it('return null object when files', () => {
-    const expected = getFileListObj(([{}, {}] as never) as FileList, false)
+    const expected = getFileListObj(
+      ([{}, {}] as never) as FileList,
+      false,
+      true
+    )
+
+    expect(expected).toBeNull()
+  })
+
+  it('return null object when files is null', () => {
+    const expected = getFileListObj(([] as never) as FileList, false, true)
 
     expect(expected).toBeNull()
   })
 
   it('return FileList object when files', () => {
-    const expected = getFileListObj(([{}, {}] as never) as FileList, true)
+    const expected = getFileListObj(
+      ([{}, {}] as never) as FileList,
+      true,
+      false
+    )
 
     expect(expected).toEqual([{}, {}])
   })
